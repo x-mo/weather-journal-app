a6e1f1065f0c999ccb385ddd469e37ee992c2ebc
Mohamed Awad <X_MohamedAhmed@yahoo.com>
2024-11-09 18:41:10 +0400

    Add initial fetch weather data

--- website/app.js
+++ website/app.js
@@ -1,13 +1,13 @@
 /* Global Variables */
 
 // Personal API Key for OpenWeatherMap API
-const apiKey = 'dcb343a416106633908dd8e70c52a5ca&units=imperial';
+const apiKey = 'appid=dcb343a416106633908dd8e70c52a5ca&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
-//fetch
+//Fetch project data
 async function getData(url) {
     const res = await fetch(url);
     try {
@@ -19,4 +19,20 @@ async function getData(url) {
     }
 }
 
+//Fetch weather data
+async function getWeather(url) {
+    
+    const res = await fetch(url+apiKey);
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+const weatherBaseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
+const zip = 'zip=11765&';
+
+// getWeather(weatherBaseUrl+zip);
 getData('/all');
